4b52a4b080e03667426f7c76e0942d0deda30999
P Nikita <92731180+nikita8763@users.noreply.github.com>
2023-06-11 20:56:39 +0530

    fisrt commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// API
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4ba401fd93mshc5bed3a6b73fd20p17146bjsnec9d948278a5',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}  
