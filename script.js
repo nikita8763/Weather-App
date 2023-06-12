8ba4158f2659e94254e919b36fdf10a5a74bd5fd
P Nikita <92731180+nikita8763@users.noreply.github.com>
2023-06-13 01:35:12 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // API
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
 const options = {
 	method: 'GET',
 	headers: {
@@ -8,10 +7,21 @@ const options = {
 	}
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}  
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi',options)
+	.then(response => response.json())
+	.then((response) => {
+		
+	console.log(response);
+
+	cloud_pct.innerHTML = response.cloud_pct
+	temp.innerHTML = response.temp 
+	feels_like.innerHTML = response.feels_like 
+	humidity.innerHTML = response.humidity 
+	min_temp.innerHTML = response.min_temp 
+	max_temp.innerHTML = response.max_temp 
+	Wind_speed.innerHTML = response.wind_speed 
+	Wind_degrees.innerHTML = response.wind_degrees 
+	sunrise.innerHTML = response.sunrise 
+	sunset.innerHTML = response.sunset
+	})
+	.catch (err => console.error(err));
